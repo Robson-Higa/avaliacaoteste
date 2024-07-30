250d0e0539a1db182994d0986e1b9ea9aeec08ba
unknown <Robson Higa>
2024-07-30 16:23:26 -0400

    m

---
+++ colorpanel/cypress/e2e/app.cy.ts
@@ -0,0 +1,20 @@
+describe("e2e tests", () => {
+  beforeEach(() => {
+    cy.visit("");
+    cy.fixture("values.json").as("values");
+  });
+
+  
+
+  it("should decrement counter correctly", function () {
+    const sentence = this.values.sentence;
+    cy.get('[data-cy="sentence-input"]').type(sentence);
+
+    for (let i = 0; i < sentence.length; i++) {
+      cy.get('[data-cy="sentence-input"]').type("{backspace}");
+    }
+
+    const result = `A frase contém 0 caracteres.`;
+    cy.get('[data-cy="counter-result"]').should("have.text", result);
+  });
+});
